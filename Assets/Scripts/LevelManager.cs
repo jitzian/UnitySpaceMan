db0d9adbb10fc472ca0a53e54bbc334196cd4a97
joNA <jitzian@gmail.com>
2022-07-21 16:51:15 -0400

    spaceMan jc - Ep. 27: Creation of level manager and mapping in different levels (3 for now) in the UI of unity

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public static LevelManager sharedInstance = null;
+
+    public List<LevelBlock> listOfLevelBlocks = new List<LevelBlock>();
+    public List<LevelBlock> currentLevelBlocks = new List<LevelBlock>();
+    public Transform levelStartPosition;
+
+
+    private void Awake()
+    {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateInitialBlocks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void addLevelBlock()
+    {
+
+    }
+
+    public void removeLevelBlock()
+    {
+
+    }
+
+    public void removeAllLevelBlocks()
+    {
+
+    }
+
+    //Initialization
+    public void generateInitialBlocks()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            addLevelBlock();
+        }
+    }
+
+}
