dba5c0f758d5beb19c2ffe7798b96748049db48b
joNA <jitzian@gmail.com>
2022-08-07 10:36:41 -0400

    spaceMan jc - Ep. 37: Fixing compilation issue and trying to implement the score and maxScore functionality

---
+++ Assets/Scripts/GameView.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class GameView : MonoBehaviour {
+    public TextMeshProUGUI coinsText, scoreText, maxScoreText;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (GameManager.sharedInstance.currentGameState == GameState.IN_GAME) {
+            Debug.Log("WE ARE IN GAMING MODE...!!!!!");
+            int coins = 0;
+            float score = 0;
+            float maxScore = 0;
+            coinsText.text = coins.ToString();
+            scoreText.text = "Score " + score.ToString("f1");
+            maxScoreText.text = "Max Score:" + maxScore.ToString("f1");
+        }
+    }
+}
